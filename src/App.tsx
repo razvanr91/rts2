68087d48e25ed29fee89e98e2db408d1e5a8885d
razvanr91 <razvan.roman91@outlook.com>
2022-01-25 23:04:10 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-
 import "./App.css";
-import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 function App() {
 	return (
 		<div className="App">
-			<GuestList />
+			<UserSearch />
 		</div>
 	);
 }
