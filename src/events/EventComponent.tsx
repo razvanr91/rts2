41981c9b79ed5cb55e1a692c3ad1caccf96e31aa
razvanr91 <razvan.roman91@outlook.com>
2022-02-06 17:03:31 +0000

    Update EventComponent.tsx

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -3,10 +3,18 @@ const EventComponent: React.FC = () => {
 		console.log(e);
 	};
 
+	const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+		console.log("I'm being dragged!");
+		console.log(e.clientX, e.clientY);
+	};
+
 	return (
 		<div>
 			<h2>Event Component</h2>
 			<input onChange={onChange} />
+			<div draggable onDragStart={onDragStart}>
+				<h2>Drag me!</h2>
+			</div>
 		</div>
 	);
 };
