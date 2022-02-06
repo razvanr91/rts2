caeadd12ca50d123852515322dc9e929a473b5d5
razvanr91 <razvan.roman91@outlook.com>
2022-02-06 03:57:30 +0000

    Update UserSearch.tsx

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -8,14 +8,30 @@ const users = [
 
 const UserSearch: React.FC = () => {
 	const [name, setName] = useState("");
+	const [user, setUser] = useState<{ name: string; age: number } | undefined>();
 
-	const onClick = () => {};
+	const onClick = () => {
+		const foundUser = users.find((user) => {
+			return user.name.toLowerCase() === name.toLowerCase();
+		});
+
+		setUser(foundUser);
+		console.log(foundUser);
+	};
 
 	return (
 		<div>
 			<h2>User Search</h2>
 			<input value={name} onChange={(e) => setName(e.target.value)} />
 			<button onClick={onClick}>Find user</button>
+			<div>
+				{user && (
+					<div>
+						<h2>User name: {user.name}</h2>
+						<h2>User age : {user.age}</h2>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
