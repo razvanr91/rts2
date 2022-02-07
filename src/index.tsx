6a0bc83ffc6da0312db8878f3e0d06cbc9d2bfc9
razvanr91 <razvan.roman91@outlook.com>
2022-02-07 11:38:40 +0000

    Reset app

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import EventComponent from "./events/EventComponent";
-import Parent from "./props/Parent";
-import UserSearch from "./refs/UserSearch";
-import GuestList from "./state/GuestList";
 
 const App = () => {
 	return (
 		<div>
 			<h1>Hi there!</h1>
-			<Parent />
-			<GuestList />
-			<UserSearch />
-			<EventComponent />
 		</div>
 	);
 };
