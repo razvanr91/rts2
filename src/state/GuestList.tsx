3bf5bccdaa0522bd71d64f971d167fd3d94276e2
razvanr91 <razvan.roman91@outlook.com>
2022-02-07 12:15:24 +0000

    Update GuestList.tsx

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 const GuestList: React.FC = () => {
+	const [name, setName] = useState("");
+	const [guests, setGuests] = useState<string[]>([]);
+
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setName(e.target.value);
+	};
+
+	const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		setGuests([...guests, name]);
+		setName("");
+	};
+
 	return (
 		<div>
 			<h2>Guest List</h2>
+			<input value={name} onChange={onChange} />
+			<button onClick={onClick}>Add guest</button>
+			{guests &&
+				guests.map((guest) => {
+					return (
+						<div key={guest}>
+							<h4>{guest}</h4>
+						</div>
+					);
+				})}
 		</div>
 	);
 };
